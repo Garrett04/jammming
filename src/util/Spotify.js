61e5a15f2c2771d758fa76a706521cf0c3f4fab6
garrett04 <garrettcorreia21@gmail.com>
2023-12-06 11:35:50 +0000

    Update Spotify.js

--- src/util/Spotify.js
+++ src/util/Spotify.js
@@ -1,5 +1,5 @@
 let clientId = process.env.REACT_APP_SPOTIFY_CLIENT_ID;
-const redirectUri = 'http://localhost:3000';
+const redirectUri = 'https://jammming-eosin-zeta.vercel.app';
 let accessToken = '';
 
 const Spotify = {
